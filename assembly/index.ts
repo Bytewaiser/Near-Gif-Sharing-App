26ccdfd8d316100131e97b89517590d2ef472130
Bytewaiser <bytewaiser@gmail.com>
2022-04-22 20:39:48 +0300

    CRUD methods implemented in smart contract

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,21 @@
-import { Gif } from "./model";
+import { Gif, PartialGif } from "./model";
 
 export function create(gifLink: string): Gif {
     return Gif.insert(gifLink);
+}
+
+export function getById(id: u32): Gif {
+    return Gif.findById(id);
+}
+
+export function get(offset: u32 = 0, limit: u32 = 10): Gif[] {
+    return Gif.find(offset, limit);
+}
+
+export function update(id: u32, updates: PartialGif): Gif {
+    return Gif.findByIdAndUpdate(id, updates);
+}
+
+export function del(id: u32): void {
+    Gif.findByIdAndDelete(id);
 }
--- assembly/model.ts
+++ assembly/model.ts
@@ -3,14 +3,20 @@ import { PersistentUnorderedMap, math } from "near-sdk-as";
 export const gifs = new PersistentUnorderedMap<u32, Gif>("todos");
 
 @nearBindgen
+export class PartialGif {
+    gifLink: string;
+    vote: u32;
+}
+
+@nearBindgen
 export class Gif {
     id: u32;
-    gif: string;
+    gifLink: string;
     vote: u32;
 
     constructor(gifLink: string) {
         this.id = math.hash32<string>(gifLink);
-        this.gif = gifLink;
+        this.gifLink = gifLink;
         this.vote = 0;
     }
 
@@ -18,5 +24,26 @@ export class Gif {
         const gif = new Gif(gifLink);
         gifs.set(gif.id, gif);
         return gif;
+    }
+
+    static findById(id: u32): Gif {
+        return gifs.getSome(id);
+    }
+
+    static find(offset: u32, limit: u32): Gif[] {
+        return gifs.values(offset, offset + limit);
+    }
+
+    static findByIdAndUpdate(id: u32, partial: PartialGif): Gif {
+        const gif = this.findById(id);
+
+        gif.gifLink = partial.gifLink;
+        gif.vote = partial.vote;
+        gifs.set(id, gif);
+        return gif;
+    }
+
+    static findByIdAndDelete(id: u32): void {
+        gifs.delete(id);
     }
 }
