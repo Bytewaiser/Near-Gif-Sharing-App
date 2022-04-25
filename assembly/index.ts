070e7cc13b92df21a185565877faad437e577145
Bytewaiser <bytewaiser@gmail.com>
2022-04-25 19:14:58 +0300

    Final Commit

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,4 +1,4 @@
-import { Gif, PartialGif } from "./model";
+import { Gif } from "./model";
 
 export function create(gifLink: string): Gif {
     return Gif.insert(gifLink);
@@ -8,22 +8,22 @@ export function getById(id: u32): Gif {
     return Gif.findById(id);
 }
 
-export function upVote(id: u32): void {
-    Gif.upVote(id);
+export function upVote(id: u32): string {
+    return Gif.upVote(id);
 }
 
-export function downVote(id: u32): void {
-    Gif.downVote(id);
+export function downVote(id: u32): string {
+    return Gif.downVote(id);
 }
 
 export function get(offset: u32 = 0, limit: u32 = 10): Gif[] {
     return Gif.find(offset, limit);
 }
 
-export function update(id: u32, updates: PartialGif): Gif {
-    return Gif.findByIdAndUpdate(id, updates);
+export function update(id: u32, gifLink: string): Gif {
+    return Gif.findByIdAndUpdate(id, gifLink);
 }
 
-export function del(id: u32): void {
-    Gif.findByIdAndDelete(id);
+export function del(id: u32): string {
+    return Gif.findByIdAndDelete(id);
 }
--- assembly/model.ts
+++ assembly/model.ts
@@ -1,19 +1,17 @@
-import { PersistentUnorderedMap, math } from "near-sdk-as";
+import { Context, PersistentUnorderedMap, math, u128, ContractPromiseBatch } from "near-sdk-as";
 
-export const gifs = new PersistentUnorderedMap<u32, Gif>("todos");
-
-@nearBindgen
-export class PartialGif {
-    gifLink: string;
-}
+export const gifs = new PersistentUnorderedMap<u32, Gif>("gifs");
+export const userGifCount = new PersistentUnorderedMap<string, u32>("usergifcount");
 
 @nearBindgen
 export class Gif {
-    id: u32;
+    userId: string;
     gifLink: string;
+    id: u32;
     vote: u32;
 
     constructor(gifLink: string) {
+        this.userId = Context.predecessor;
         this.id = math.hash32<string>(gifLink);
         this.gifLink = gifLink;
         this.vote = 0;
@@ -21,7 +19,19 @@ export class Gif {
 
     static insert(gifLink: string): Gif {
         const gif = new Gif(gifLink);
+
+        const gifCount = userGifCount.contains(gif.userId) ? userGifCount.getSome(gif.userId) : 0;
+        if (gifCount == 0) userGifCount.set(gif.userId, 0);
+
+        if (gifCount >= 1) {
+            //const baseMoney = u128.from(Math.pow(10, 23).toString());
+            const baseMoney = u128.from("100000000000000000000000");
+            assert(Context.attachedDeposit >= baseMoney, "You can not create more than 1 gif free. 0.1 Near is needed for extra creation of gifs.");
+        }
+
         gifs.set(gif.id, gif);
+        userGifCount.set(gif.userId, userGifCount.getSome(gif.userId) + 1);
+
         return gif;
     }
 
@@ -29,32 +39,52 @@ export class Gif {
         return gifs.getSome(id);
     }
 
-    static upVote(id: u32): void {
+    static upVote(id: u32): string {
         const gif = Gif.findById(id);
+        const baseMoney = u128.from("10000000000000000000000");
+
+        assert(gif.userId != Context.predecessor, "You can not vote your own Gif");
+        assert(Context.attachedDeposit >= baseMoney, "You need at least 0.01 Near to vote a Gif");
+        const toRecipient = ContractPromiseBatch.create(gif.userId);
+        toRecipient.transfer(Context.attachedDeposit);
+
         gif.vote += 1;
         gifs.set(id, gif);
+        return `Upvoted and sent ${Context.attachedDeposit} Near from ${Context.sender} to ${gif.userId}`;
     }
 
-    static downVote(id: u32): void {
+    static downVote(id: u32): string {
         const gif = Gif.findById(id);
+        const baseMoney = u128.from("10000000000000000000000");
+
+        assert(gif.userId != Context.predecessor, "You can not vote your own Gif");
+        assert(Context.attachedDeposit >= baseMoney, "You need at least 0.01 Near to vote a Gif");
+        const toRecipient = ContractPromiseBatch.create(gif.userId);
+        toRecipient.transfer(Context.attachedDeposit);
+
         gif.vote = <u32>Math.max(0, gif.vote - 1);
         gifs.set(id, gif);
+        return `Downvoted and sent ${Context.attachedDeposit} Near from ${Context.sender} to ${gif.userId}`;
     }
 
     static find(offset: u32, limit: u32): Gif[] {
         return gifs.values(offset, offset + limit);
     }
 
-    static findByIdAndUpdate(id: u32, partial: PartialGif): Gif {
+    static findByIdAndUpdate(id: u32, gifLink: string): Gif {
         const gif = this.findById(id);
+        assert(gif.userId == Context.predecessor, "You are not the owner of the Gif");
 
-        gif.gifLink = partial.gifLink;
+        gif.gifLink = gifLink;
         gif.vote = 0;
         gifs.set(id, gif);
         return gif;
     }
 
-    static findByIdAndDelete(id: u32): void {
+    static findByIdAndDelete(id: u32): string {
+        const gif = this.findById(id);
+        assert(gif.userId == Context.predecessor, "You are not the owner of the Gif");
         gifs.delete(id);
+        return `Deleted Gif with the id: ${id}`;
     }
 }
